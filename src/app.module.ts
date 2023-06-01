c175cda0ee82041df9a3456ebfe15eecb6908e2e
yang <1372201820@qq.com>
2023-06-01 20:53:06 +0800

    feat:orm框架接入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { GirlModule } from './girl/girl.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GirlController } from './girl/girl.controller';
+import { GirlService } from './girl/girl.service';
 
 @Module({
-  imports: [GirlModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 数据库类型
+      host: 'localhost', // 数据库的连接地址host
+      port: 3306, // 数据库的端口 3306
+      username: 'root', // 连接账号
+      password: 'root', // 连接密码
+      database: 'test_db', // 连接的表名
+      retryDelay: 500, // 重试连接数据库间隔
+      retryAttempts: 10, // 允许重连次数
+    }),
+  ],
+  controllers: [GirlController],
+  providers: [GirlService],
 })
 export class AppModule {}

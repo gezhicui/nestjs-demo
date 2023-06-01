037dc44459c5ff41cabcaae8ebbb1cba56b93b0d
yang <1372201820@qq.com>
2023-06-01 20:40:34 +0800

    feat:基础请求方式

--- src/girl/girl.controller.ts
+++ src/girl/girl.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Query,
+  Body,
+  Param,
+  Headers,
+} from '@nestjs/common';
+import { GirlService } from './girl.service';
 
 @Controller('girl')
 export class GirlController {
+  constructor(private girlService: GirlService) {}
   @Get()
   getGirls(): any {
-    return {
-      code: 0,
-      data: ['翠花', '小红', '大丫', '测试11'],
-      msg: '请求女孩列表成功',
-    };
+    return this.girlService.getGirls();
+  }
+
+  //------新增Post请求方法
+  @Post('/add')
+  addGirl(@Body() body): any {
+    console.log(body);
+    return this.girlService.addGirl();
+  }
+
+  @Get('/getGirlById')
+  getGirlById(@Query() query): any {
+    console.log('req', query);
+    //因为通过Get方式传递过来的是字符串，所有我们需要用parseInt转化为数字
+    const id: number = parseInt(query.id);
+    return this.girlService.getGirlById(id);
+  }
+
+  @Get('/findGirlById/:id/:name')
+  findGirlById(@Param() params, @Headers() header): any {
+    console.log(params.name);
+    console.log(header);
+    const id: number = parseInt(params.id);
+    return this.girlService.getGirlById(id);
   }
 }
